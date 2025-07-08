3ebf7be4ba510ae493981e8b811e7ee14c5faa1c
dozyio <github@dozy.io>
2025-07-08 16:50:56 +0100

    check for ci

--- packages/gui/next.config.ts
+++ packages/gui/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: process.env.GITHUB_PAGES ? '/chess-bot' : '',
+  basePath: process.env.CI ? '/chess-bot' : '',
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
